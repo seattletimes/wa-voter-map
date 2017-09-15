aff7167607bd8ebf1bd37dc9f836a1e4e477d20e
Thomas Wilburn <tbone@thomaswilburn.net>
2017-09-15 09:22:14 -0700

    Remove commented-out code since I keep using this as an example

--- src/js/main.js
+++ src/js/main.js
@@ -4,54 +4,9 @@
 
 require("component-responsive-frame/child");
 var qsa = require("./lib/qsa.js");
-// require("component-leaflet-map");
-// var savage = require("savage-query");
-
-// var data = require("./presidential_vote_by_county.geo.json");
-
-// var mapElement = document.querySelector("leaflet-map");
-// var L = mapElement.leaflet;
-// var map = mapElement.map;
-
-// map.scrollWheelZoom.disable();
-// map.touchZoom.disable();
-// map.doubleClickZoom.disable();
-// map.boxZoom.disable();
-// map.removeControl(map.zoomControl);
-// map.dragging.disable();
 
 var year = 1968;
 var delay;
-
-// function getColor(p, m) {
-//   if (p == "D") {
-//     // return '#045a8d';
-//     return m >= .7 ? '#045a8d' :
-//        m >= .6 ? '#2b8cbe' :
-//        m >= .5 ? '#74a9cf' :
-//        m >= .4 ? '#a6bddb' :
-//        '#d0d1e6';
-//   } else if (p == "R") {
-//     // return '#b30000';
-//     return m >= .7 ? '#b30000' :
-//        m >= .6 ? '#e34a33' :
-//        m >= .5 ? '#fc8d59' :
-//        m >= .4 ? '#fdbb84' :
-//        '#fdd49e';
-//   }
-  //   if (p == "D") {
-  //          return m >= .55 ? '#2b8cbe' :
-  //      m >= .5 ? '#74a9cf' :
-  //      '#bdc9e1' ;
-  // } else if (p == "R") {
-  //   return m >= .55 ? '#e34a33' :
-  //      m >= .5 ? '#fc8d59' :
-  //      '#fdcc8a' ;
-  // } else if (p == "T") {
-  //   return "white";
-  // }
-
-// };
 
 var colorMap = function() {
   countyData.forEach(function(row) {
@@ -68,37 +23,9 @@ var colorMap = function() {
     var shape = document.querySelector("#" + county);
 
     var className = getClassName(party, margin);
-    // classes.forEach(function(class) {
-    //   savage(shape).removeClass(class);
-    // });
-    // savage(shape).addClass(className);
     shape.setAttribute("class", `cls-1 ${className}`);
   })
 };
-
-// function style(feature) {
-//   var party = feature.properties[year + "_Party"];
-//   var color;
-//   if (party == "T") {
-//     color = "#AAA";
-//   } else {
-//     var margin = feature.properties[year + "_" + party];
-//     color = getColor(party, margin);
-//   }
-//   return {
-//     fillColor: color,
-//     weight: 0.5,
-//     opacity: 1,
-//     color: 'white',
-//     fillOpacity: 1
-//   };
-// }
-
-// var geojson = L.geoJson(data, {
-//   style: style
-// }).addTo(map);
-
-// map.fitBounds(geojson.getBounds());
 
 var animate = function() {
   if (year == 2008) {
